b8e793d311e86ff6388d04c98223633544b813f0
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-09-21 09:00:32 +0300

    COmpleted assignment

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -5,7 +5,7 @@ import React from "react";
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
-  render(<Article />);
+	render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+	expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
